851c4b22e09e7f031fa62e8610a388056589e53d
Attila Toth <hello@attilatoth.dev>
2021-10-27 22:22:30 +0200

    Add config file

---
+++ config.py
@@ -0,0 +1,9 @@
+# Make sure to modify these settings before running the ingestion script!
+
+DB_NAME="tsdb"
+DB_HOST="something.tsdb.cloud.timescale.com"
+DB_USER="tsdbadmin"
+DB_PASS="passwd"
+DB_PORT="66666"
+OPENSEA_START_DATE="2021-10-01T00:00:00"
+OPENSEA_END_DATE="2021-10-07T23:59:59"
